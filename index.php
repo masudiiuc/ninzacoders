a9675b510b81ef394896e4d516bed38fd9175e3c
Masud <masudiiuc@gmail.com>
2013-02-19 00:35:29 +0600

    initial deploy

---
+++ index.php
@@ -0,0 +1,187 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Template &middot; Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Domine' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+      
+    </style>
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <div class="masthead">
+        <h3 class="muted">Ninza Coders</h3>
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="container">
+              <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Downloads</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div><!-- /.navbar -->
+      </div>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Meet The Ninja's</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <a class="btn btn-large btn-success" href="#">Get started today</a>
+      </div>
+
+      <hr>
+
+      <!-- Example row of columns -->
+      <div class="row-fluid">
+        <div class="span4 box">
+          <h2>M.M.H.Masud</h2>
+          <p>Framework Ninja (Codeigniter, Symfony2.0, Zend2.0 etc.)</p>
+          <p><a class="btn" href="mailto:masudiiuc@gmail.com">Contact Now &raquo;</a></p>
+        </div>
+        <div class="span4 box">
+          <h2>Asadur Rahman</h2>
+          <p>Javascript Ninja (Backbone.js, Node.js, Phantom.js, Ember.js) </p>
+          <p><a class="btn" href="mailto:asad.dk.bd@gmail.com">Contact Now &raquo;</a></p>
+       </div>
+        <div class="span4 box">
+          <h2>Raju Mazumder</h2>
+          <p>Platform Ninja (Ruby, PHP, Java, .NET etc.)</p>
+          <p><a class="btn" href="mailto:rajuniit@gmail.com">Contact Now &raquo;</a></p>
+        </div>
+
+      </div>
+      <br/>
+      <div class="row-fluid">
+        <div class="span4 box">
+          <h2>Emran Hasan</h2>
+          <p>Platform Ninja, Web Architect, Consultant</p>
+          <p><a class="btn" href="mailto:phpfour@gmail.com">Contact Now &raquo;</a></p>
+        </div>
+        <div class="span4 box">
+          <h2>Saif Noman</h2>
+          <p>Embeded & Web Ninja (Backbone.js, Node.js, Phantom.js, Ember.js) </p>
+          <p><a class="btn" href="mailto:asad.dk.bd@gmail.com">Contact Now &raquo;</a></p>
+       </div>
+        <div class="span4 box">
+          <h2>Anis U. Ahmed</h2>
+          <p>Platform Ninja (Ruby, PHP, Java, .NET etc.)</p>
+          <p><a class="btn" href="mailto:rajuniit@gmail.com">Contact Now &raquo;</a></p>
+        </div>
+        
+      </div>
+
+      <hr>
+
+      
+
+    </div> <!-- /container -->
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+        
+        <div class="span4">
+          
+          <h3><i class="icon-sitemap icon-large"></i> Links</h3>
+          
+          
+          <!-- <ul class="footer-links unstyled clearfix">
+                        <li><a href="./">Home</a></li>
+                        <li><a href="#">Mauris accumsan</a></li>
+                        <li><a href="#">Lauris</a></li>
+                        <li><a href="#">Accumsan</a></li>
+                        
+                    </ul>
+                    
+                    <ul class="footer-links unstyled clearfix">    
+                        <li><a href="#">Hello World</a></li>
+                        <li><a href="#">Mauris accumsan</a></li>
+                        <li><a href="#">Lauris</a></li>
+                        <li><a href="#">Accumsan</a></li>
+                        
+                    </ul> -->
+          
+        </div> <!-- /span4 -->
+        
+        
+        <div class="span4">
+          
+          <h3><i class="icon-thumbs-up icon-large"></i> Connect With Us</h3>
+          
+          
+          <p>We know you have at least one social profile... We would love to hear from you!</p>
+
+          <div class="footer-social">
+                <a href="#"><i class="icon-pinterest-sign"></i></a>
+               <a href="#"><i class="icon-twitter-sign"></i></a>
+               <a href="#"><i class="icon-facebook-sign"></i></a>
+               <a href="#"><i class="icon-google-plus-sign"></i></a>
+               <a href="#"><i class="icon-linkedin-sign"></i></a>
+          </div>
+          
+       
+          
+        </div> <!-- /span4 -->
+        
+        <div class="span4">
+          
+          <h3><i class="icon-envelope-alt icon-large"></i> Subscribe</h3>
+          
+
+          <p>Subscribe to our newsletter to get the latest news!</p>
+          
+          
+          <form>
+            
+            <div class="input-prepend">
+            <span class="add-on"><i class="icon-envelope"></i></span><input class="span2" id="prependedInput" size="16" type="text" placeholder="Your Email">
+          </div>
+            
+            <button class="btn ">Subscribe</button>
+          </form>
+          
+          
+        </div> <!-- /span4 -->
+        
+      </div>
+      
+      </div>
+    </footer>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.8.2.min.js"></script>  
+    <script src="js/bootstrap.min.js"></script>  
+</body>
+</html>
